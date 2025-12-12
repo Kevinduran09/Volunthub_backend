5068a1925859c0d3605001adffef8fe8132ccea9
kevinduran09 <kevinduran.net.123@gmail.com>
2025-12-11 21:52:51 -0600

    Create: Base template repository

---
+++ app/repositories/base_repository.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar, List, Optional
+
+
+T = TypeVar("T")  # Generic type model
+
+
+class BaseRepository(ABC, Generic[T]):
+    """
+    Define the structure of all repositories that implement this base repository
+    """
+    @abstractmethod
+    async def get_all(self) -> List[T]:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, id: int) -> Optional[T]:
+        pass
+
+    @abstractmethod
+    async def create(self, entity: T) -> T:
+        pass
+
+    @abstractmethod
+    async def update(self, id: int, entity: T) -> T:
+        pass
+
+    @abstractmethod
+    async def delete(self, id: int) -> None:
+        pass
