a35922f8d2948380082a50991a310de0c031cbd6
kevinduran09 <kevinduran.net.123@gmail.com>
2026-01-05 22:49:16 -0600

    Create main point for fastapi application

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
 
-app = FastAPI()
+from app.graphql.schema import schema
+from app.graphql.context import get_context
+
+import app.infrastructure.models
+app = FastAPI(
+    title="Volunthub Backend v2.0",
+    summary="Migracion del backend de Volunthub desde express a fastapi con base de datos local postgres",
+)
+
+graphql_app = GraphQLRouter(schema, context_getter=get_context)
+app.include_router(graphql_app, prefix="/graphql")
 
 
 @app.get("/")
